87aff9833a82ff309a2fb948e043c99c3894f83b
Vaibhav Singh <singhvaibhav654@gmail.com>
2024-09-16 18:45:00 +0530

    first commit

---
+++ firstcl.js
@@ -0,0 +1,139 @@
+// let 
+// var
+// const
+
+
+// let x=10;
+
+// let x=5;
+
+{
+
+let io=9;
+
+}
+
+// console.log(io)
+
+// console.log("hellow");
+
+// window.alert("helo");
+
+
+// const pie=3.14;
+//  pie=9;
+
+// let p=8;
+//  console.log(p++);
+//  console.log(p);
+// console.log(p--);
+// console.log(p)
+
+let i=9;
+let y=8;
+
+
+// i-=y; // i=i-y
+// i+=y; // i=i+y
+// i*=y; // i=i*y
+// i/=y; // i=i/y
+
+
+// console.log(8>7 && 7>6);
+// ||
+
+// 8
+// 9
+// 9
+
+
+// if(6>7 || 7>6 ){
+//     console.log(true)
+// }
+// let p =9;
+
+// if(p>18){
+//     console.log("Can go");
+// }
+// else{
+//     console.log("Can't go");
+// }
+
+// var age=19;
+// var x="vaccinated";
+
+// if(age>18 && x==="vaccinated"){
+//     console.log("He can go")
+// }else{
+//     console.log("he can't go")
+// }
+
+
+
+
+// let ui=[  [1,2,3],
+//           [4,5,6],
+//           [7,8,9]   ];
+
+
+// let u=[5,5,8,9,7];
+
+// let ui=[[1,2,3],[4,5,6],[7,8,9]];
+
+// for(i=0;i<3;i++){
+//     for(j=0;j<3;j++){
+//         console.log(ui[i][j])
+//     }
+// }
+
+
+function sec(){
+    console.log("this is second function")
+}
+
+function uiop(){
+
+    console.log("hello this is first function");
+    sec();
+
+}
+
+
+
+
+uiop()
+
+
+
+
+// function ty(g,h){
+ 
+//     // console.log(g,h)
+//     console.log(g+h)
+// }
+
+
+
+
+// ty(5,3)
+
+
+
+function ty(yu){
+    console.log(yu)
+}
+
+
+
+
+let ui = {
+    length:9,
+    width:10,
+    color:"blue"
+}
+
+ty(ui)
+
+
+// console.log(ui.color)
+// console.log(ui[color])
